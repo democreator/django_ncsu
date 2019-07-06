4d16b5249a7119d67c3011cd010cf4849e51d9de
Max Haeberlein <max.haeberlein@intel.com>
2019-07-06 14:29:57 +0200

    Allow HOST localhost

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'qzkuf51df5zz^w#_3b0z8o4a&f#d4+7n6rtv8ed+zax8x31_ih'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1',]
 
 
 # Application definition
