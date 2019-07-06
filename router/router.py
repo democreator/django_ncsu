31809d2bbd3b5eaf0858e00253f77ec9a6f1af81
Max Haeberlein <max.haeberlein@intel.com>
2019-07-06 14:58:04 +0200

    Add skeleton function for router

---
+++ router/router.py
@@ -0,0 +1,12 @@
+# Entry point for NCSU code
+
+import json
+
+def router(json_input):
+    ''' This function should '''
+    dict_input = json.loads(json_input)
+
+    # do something with input
+
+    dict_output = {'route1':[(0,0), (1,1)]}
+    return dict_output
